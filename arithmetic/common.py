dcfd4c66ae6d7b85f48d2a487715efe2ce8e3f5c
jiang <shaojie.jiang@aispeech.com>
2020-12-13 22:55:45 +0800

    add isAnagram

--- arithmetic/common.py
+++ arithmetic/common.py
@@ -21,3 +21,27 @@ class Solution:
     def maxSlidingWindow1(self, nums: List[int], k: int) -> List[int]:
         pass
 
+    def isAnagram(self, s: str, t: str) -> bool:
+        # #https: // leetcode - cn.com / problems / valid - anagram / submissions /
+        return sorted(s)==sorted(t)
+    def isAnagram_map(self,s:str,t:str) -> bool:
+        dic1={}
+        dic2={}
+        for item in s:
+            dic1[item]=dic1.get(item,0)+1
+        for item in t:
+            dic2[item]=dic2.get(item,0)+1
+        return dic1==dic2
+    def isAnagram_map_1(self,s:str,t:str)->bool:
+        dic1=[0]*26
+        dic2=[0]*26
+        for item in s:
+            dic1[ord(item)-ord("a")]+=1
+        for item in t:
+            dic2[ord(item)-ord("a")]+=1
+        return dic1==dic2
+
+
+
+
+
